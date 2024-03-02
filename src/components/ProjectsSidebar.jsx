653fe1d842568ad6c21440809026974c989d357a
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-02 17:15:58 +0100

    Project sidebar added

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,16 @@
+export default function ProjectsSidebar() {
+
+    return (
+        <aside>
+        <h2>Your Project</h2>
+        <div>
+            <button>
+                + Add Project
+            </button>
+        </div>
+        <ul>
+
+        </ul>
+        </aside>
+    );
+}
