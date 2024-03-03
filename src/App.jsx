8a0d5f581c7a1d2bfde32da48948268f51d28fae
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-03 10:22:52 +0100

    Project deletion done

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,18 @@ function App() {
     });
   }
 
+  function handleDelete() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+          ),
+      };
+    });
+  }
+
   function handeleAddProject(projectData) {
     setProjectsState(prevState => {
       const projectId = Math.random();
@@ -55,7 +67,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDelete} />;
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handeleAddProject} onCancel={handleCancelAddProject} />
